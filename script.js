8c8a8f5c7777e6d457fb7c17d9a5f231c5c045f5
kessedias <kesse.dias97@outlook.com>
2020-09-24 08:59:31 -0300

    :sparkles: feat: Adicionando classe de transition do texto

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
      $(document).keypress(function(e) {
          if (e.wich == 13 || e.keyCode == 13) {
              $(".phrase").text("Uma frase aqui");
+             $(".phrase").addClass("phrase-animation");
          }
      });
  });
