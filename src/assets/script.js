c81d543db7fd37e9dd09edd0e91b8c20123a4b05
kessedias <kesse.dias97@outlook.com>
2020-09-24 14:05:32 -0300

    :sparkles: feat: Implementando easter egg

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,7 +1,9 @@
+console.log("❤️❤️❤️Eu te adoro, Ane ❤️❤️❤️");
+console.log("Você mora aqui -> ❤️");
 $(document).ready(function() {
     $(document).keypress(function(e) {
         if (e.wich == 13 || e.keyCode == 13) {
-            $(".phrase").text("Uma frase aqui");
+            $(".phrase").text("Quer namorar comigo?");
             $(".phrase").addClass("phrase-animation");
         }
     });
